1824964a11395786c9fcc8efd1c5df5962df870f
yabessy <johanliebert.outergod@gmail.com>
2022-12-19 09:07:36 +0700

    get chat list from firesore and show it

--- components/chatlists/ChatList.tsx
+++ components/chatlists/ChatList.tsx
@@ -1,19 +1,14 @@
-import React from "react"
+import React from "react";
 
-export default function ChatList({ user }: any) {
+export default function ChatList({ user, random }: any) {
   return (
     <>
-      <img
-        className="rounded-full mr-3 w-10 h-10 object-contain cursor-pointer"
-        src={user.photoURL || ""}
-        referrerPolicy="no-referrer"
-        alt=""
-      />
+      <img className="rounded-full mr-3 w-10 h-10 object-contain cursor-pointer" src={user ? user.data().photoURL : random.photoURL} referrerPolicy="no-referrer" alt="" />
       <div className="flex flex-col items-start flex-grow leading-tight">
-        <h1 className="text-[16px]">{user.name}</h1>
+        <h1 className="text-[16px]">{user ? user.name : random.name}</h1>
         <p className="text-[14px] text-gray-600">last message</p>
       </div>
       <span className="mt-auto text-[15px] text-gray-600">17h ago</span>
     </>
-  )
+  );
 }
--- components/chatlists/ChatLists.tsx
+++ components/chatlists/ChatLists.tsx
@@ -1,87 +1,104 @@
-import { addDoc, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, where } from "firebase/firestore"
-import { useState } from "react"
-import { useRecoilState } from "recoil"
-import { auth, db } from "../../firebase"
-import { userState } from "../../atom/userAtom"
-import ChatList from "./ChatList"
-import { useRouter } from "next/router"
+import { addDoc, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { auth, db } from "../../firebase";
+import { userState } from "../../atom/userAtom";
+import ChatList from "./ChatList";
+import { useRouter } from "next/router";
+import { map } from "@firebase/util";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
-export default function Chatlists({ isUserHadChat }: any) {
-  const router = useRouter()
-  const [randomUser, setRandomUser] = useState(null)
-  const [currentUser] = useRecoilState(userState)
+export default function Chatlists() {
+  const router = useRouter();
+  const [randomUser, setRandomUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [currentUser] = useRecoilState(userState);
+
+  useEffect(() => {
+    const unsub = getDocs(collection(db, "users", currentUser.uid, "chats")).then((snapshot) => {
+      setChats(snapshot.docs);
+    });
+  }, [db]);
+
   async function searchRandomFriend() {
-    console.log("searching...")
-    const usersRef = collection(db, "users")
+    console.log("searching...");
+    const usersRef = collection(db, "users");
     // @ts-ignore
-    const q = query(usersRef, where("uid", "!=", currentUser?.uid))
-    const querySnapshot = await getDocs(q)
+    const q = query(usersRef, where("uid", "!=", currentUser?.uid));
+    const querySnapshot = await getDocs(q);
     try {
-      const randomNum = Math.floor(Math.random() * querySnapshot.docs.length)
+      const randomNum = Math.floor(Math.random() * querySnapshot.docs.length);
       // @ts-ignore
-      setRandomUser(querySnapshot.docs[randomNum].data())
+      setRandomUser(querySnapshot.docs[randomNum].data());
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
-  async function openChatRoom() {
-    const docRef = doc(db, "users", currentUser.uid, "chats", randomUser.uid)
-    const docSnap = await getDoc(docRef)
+  async function openRChatRoom() {
+    const docRef = doc(db, "users", currentUser.uid, "chats", randomUser.uid);
+    const docSnap = await getDoc(docRef);
     if (!docSnap.exists()) {
       await setDoc(docRef, {
         uid: randomUser.uid,
         name: randomUser.name,
-        photoURL: randomUser.photoURL
-      })
+        photoURL: randomUser.photoURL,
+      });
+      await setDoc(doc(db, "users", randomUser.uid, "chats", currentUser.uid), {
+        uid: currentUser.uid,
+        name: currentUser.name,
+        photoURL: currentUser.photoURL,
+      });
       await addDoc(collection(db, "users", currentUser.uid, "chats", randomUser.uid, "messages"), {
         uid: currentUser.uid,
         message: "Hello",
-        timestamp: serverTimestamp()
+        timestamp: serverTimestamp(),
       })
         .then(() => router.push(`/chatroom?theirID=${randomUser.uid}`))
-        .catch((e) => console.log(e))
+        .catch((e) => console.log(e));
     } else {
-      router.push(`/chatroom?theirID=${randomUser.uid}`)
+      router.push(`/chatroom?theirID=${randomUser.uid}`);
     }
+  }
+  async function openChatRoom(chat: any) {
+    router.push(`/chatroom?theirID=${chat.id}`);
   }
   return (
-    <>
-      {isUserHadChat ? (
-        <div>Chatlist</div>
-      ) : (
-        <div className="w-full font-mono sm:px-5 lg:px-10 pb-10 border-b border-gray-600">
-          <div className="w-full flex flex-col items-center mb-4 px-4 py-2">
-            <h1>Let's go find new friend</h1>
-            <button onClick={() => searchRandomFriend()} className="btn w-64">
-              Search 1 random friend{" "}
-            </button>
-          </div>
-          {randomUser && (
-            <div onClick={() => openChatRoom()} className="w-full h-auto flex justify-between items-center px-4 py-2 rounded-md bg-blue-100 cursor-pointer shadow hover:bg-blue-200 hover:shadow-sm">
-              <ChatList user={randomUser} />
-            </div>
-          )}
+    <div className="w-full font-mono px-4">
+      <div className="w-full  sm:px-5 lg:px-10 pb-10 border-b border-gray-600">
+        <div className="w-full flex flex-col items-center mb-4  py-2">
+          <h1>Let's go find new friend</h1>
+          <button onClick={() => searchRandomFriend()} className="btn w-64">
+            Search 1 random friend{" "}
+          </button>
         </div>
-      )}
-    </>
-  )
-}
-
-export async function getServerSideProps(context: any) {
-  if (auth.currentUser) {
-    const docRef = doc(db, "users", auth.currentUser.uid, "chats")
-    const docSnap = await getDoc(docRef)
-    if (docSnap.exists()) {
-      return {
-        props: {
-          isUserHadChat: true
-        }
-      }
-    }
-  }
-  return {
-    props: {
-      isUserHadChat: false
-    }
-  }
+        {randomUser && (
+          <div
+            onClick={() => openRChatRoom()}
+            className="relative w-full h-auto flex justify-between items-center px-4 py-2 rounded-md bg-blue-100 cursor-pointer shadow hover:bg-blue-200 hover:shadow-sm">
+            <ChatList random={randomUser} />
+            <XMarkIcon
+              className="h-6 w-6 absolute right-2 top-2 text-gray-500 cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                setRandomUser(null);
+              }}
+            />
+          </div>
+        )}
+      </div>
+      <div className="mt-2">
+        {chats.length > 0 && (
+          <div className="">
+            {chats.map((chat) => (
+              <div
+                onClick={() => openChatRoom(chat)}
+                className="w-full h-auto flex justify-between items-center px-4 py-2 rounded-md bg-blue-100 cursor-pointer shadow hover:bg-blue-200 hover:shadow-sm">
+                <ChatList user={chat} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
